56890d0d32aa205b72e740f19307f246aa7058cf
Tan H. Nguyen <thnguyn2@illinois.edu>
2015-08-26 23:47:38 -0500

    Editted fval to include TV prior

--- fval.m
+++ fval.m
@@ -1,11 +1,18 @@
 function obj  = fval(gamma,hf,tk,gk,params)
    %Compute the objective function E=||gamma-tk.gk||^2+lambda*||g-t*h||^2 +
     lambda = params.lambda;
+    tv_weight = params.tv_weight;
     F = params.F;
+    D = params.D;
+    %Measurement error
     evect1=gamma-tk.*conj(gk);
-    obj1 = sum(abs(evect1(:)).^2);
+    obj1 = sum(evect1(:).*conj(evect1(:)));
+    %Mismatching error
     evect2 = gk-F'*(hf.*(F*tk));
-    obj2 = sum(abs(evect2(:)).^2);
-    %Compute the Total Variation part of the signal
-    obj = obj1 + lambda*obj2;
+    obj2 = lambda*sum(evect2(:).*conj(evect2(:)));
+    grad_res = D*tk;
+    %Total Variation part
+    tv = (abs(grad_res(:,:,1)).^2+abs(grad_res(:,:,2)).^2+1e-15).^0.5;
+    obj3 = tv_weight*sum(tv(:));
+    obj = obj1 + obj2 + obj3;
 end
